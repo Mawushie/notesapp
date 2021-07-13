f2af453ed4572704aac1622bb47378eb828e168d
Mawushie <afi.mawushie.honyah@gmail.com>
2021-07-13 13:43:46 +0000

    log out button added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { logOutAction } from '../actions/authActions'
 import AddNote from './AddNote'
 import NotesList from './NotesList'
 
-export default class HomePage extends Component {
+
+class HomePage extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    handleLogOut =() =>{
+        this.props.logOutAction()
+    }
+
     render() {
         return (
             <div>
                 <div className = "container">
+                    <div className = 'row text-right'  style ={{padding : 20}}>
+                        <button type = 'button' onClick = {this.handleLogOut}>Log Out</button>
+                    </div>
                     <div className = "row">
                          <h3>My Notes</h3>
                         <div className = "col-md-4 notes">
-                            
                             <AddNote />
                         </div>
 
@@ -26,3 +39,9 @@ export default class HomePage extends Component {
         )
     }
 }
+
+const mapDispatchToProps = {
+    logOutAction
+}
+
+export default connect(null, mapDispatchToProps)(HomePage)
